7465f83e00c5d8dea03ca24b1ee44a3bb5b6bb50
lZh1r <electroxhackerpig09@gmail.com>
2025-04-07 09:15:51 +0300

    John Pork Lore Test done

--- src/utils/Quizes.tsx
+++ src/utils/Quizes.tsx
@@ -45,7 +45,77 @@ const sampleTest:QuizQuestion[] = [
             "Glorbo",
             "Trippi Troppi"
         ]
-    }
+    },
+    {
+        text: "Who is Subaru?",
+        correctOption: "Patrick Star",
+        options: [
+            "Patrick Star",
+            "Spongebob",
+            "Squidward",
+            "Mr. Crabs"
+        ]
+    },
+    {
+        text: "HAHA! This is John Pork Lore Test now! Why are John Pork and Tim Cheese enemies?",
+        correctOption: "John Pork took Tim's Wife",
+        options: [
+            "John Pork took Tim's Wife",
+            "Racism",
+            "Tim Cheese killed John's Family",
+            "Bob Bacon betrayed Tim Cheese"
+        ]
+    },
+    {
+        text: "Who was Tim's and John's Third Friend?",
+        correctOption: "Simon Claw",
+        options: [
+            "Simon Claw",
+            "Bob Bacon",
+            "Pengu",
+            "Henry Butter"
+        ]
+    },
+    {
+        text: "How was Tim Cheese's Life Destroyed",
+        correctOption: "He got fed up with Bob Bacon's bs after Simon Claw got caught and Tim became homeless",
+        options: [
+            "He got fed up with Bob Bacon's bs after Simon Claw got caught and became homeless",
+            "John Pork killed Simon and injured Tim badly during a fierce fight",
+            "The IRS started investigating Bob Bacon's business, which lead to his imprisonment and Tim's poverty",
+            "Tim Cheese Respect Button"
+        ]
+    },
+    {
+        text: "After John's death who decided to avenge him?",
+        correctOption: "Marvin Beak and Pengu",
+        options: [
+            "Simon Claw",
+            "Marvin Beak and Pengu",
+            "Haram Pig and Agent 5.5",
+            "Poppy the Pigeon and Friends"
+        ]
+    },
+    {
+        text: "What is the name of Pengu's partner?",
+        correctOption: "Polly",
+        options: [
+            "Polly",
+            "Molly",
+            "Holly",
+            "Dolly"
+        ]
+    },
+    {
+        text: "Who killed Pengu's partner?",
+        correctOption: "Simon Claw",
+        options: [
+            "Simon Claw",
+            "Tim Cheese",
+            "Bob Bacon",
+            "Agent 5.5"
+        ]
+    },
 ];
 
 export const quizes: Quiz[] = [
