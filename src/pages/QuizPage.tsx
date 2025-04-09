968504876d18c1569ae69bd7652730fbd9214392
lZh1r <electroxhackerpig09@gmail.com>
2025-04-09 18:17:08 +0300

    Better question top layout

--- src/pages/QuizPage.tsx
+++ src/pages/QuizPage.tsx
@@ -8,14 +8,12 @@ export default function QuizPage({quizName, questions}: {quizName: string, quest
 
     return (
         <div>
-            <div className="flex justify-between">
+            <div>
                 <Link to={'/'}>
                     <FontAwesomeIcon icon={faHouse} className="md:text-4xl text-3xl text-gray-400 outline-solid outline-2 outline-gray-400 rounded-4xl p-2
-            cursor-pointer hover:outline-white hover:text-white m-2" />
+            cursor-pointer hover:outline-white hover:text-white m-2 float-left"/>
                 </Link>
-                <h1 className="font-primary md:text-4xl text-3xl text-white text-center p-3">{quizName}</h1>
-                {/*TODO: make adequate layout*/}
-                <FontAwesomeIcon icon={faHouse} className="md:text-4xl text-3xl text-dark rounded-4xl p-2 m-2" />
+                <h1 className="font-primary md:text-4xl text-3xl text-white text-center p-3 place-self-center">{quizName}</h1>
             </div>
             <QuestionCard questions={questions}/>
         </div>
